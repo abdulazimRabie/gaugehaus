c9d995700017b4beea460dc7f41bcc3b3d5e2090
abdulazim <abdorabie14@gmail.com>
2025-06-13 04:17:56 +0300

    fix: read locations

--- utils/validateCity.js
+++ utils/validateCity.js
@@ -2,6 +2,6 @@ const fs = require("fs/promises");
 const slugify = require("slugify");
 
 module.exports = async (city) => {
-    const cities = JSON.parse(await fs.readFile(`${__dirname}/../../locations.json`)).locations;
+    const cities = JSON.parse(await fs.readFile(`${__dirname}/../locations.json`)).locations;
     return cities.some(validCity => slugify(validCity, { lower: true }) === slugify(city, { lower: true }))
 }
